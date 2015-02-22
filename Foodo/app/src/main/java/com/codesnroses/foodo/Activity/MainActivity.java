b2439234a99ee0b2434b1701b9e95dad1860ae00
Zhan <zhanhui913@gmail.com>
2015-02-21 22:04:23 -0800

    Added POST to server

--- Foodo/app/src/main/java/com/codesnroses/foodo/Activity/MainActivity.java
+++ Foodo/app/src/main/java/com/codesnroses/foodo/Activity/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.provider.Settings.Secure;
 
 import com.codesnroses.foodo.Fragment.FatsFragment;
 import com.codesnroses.foodo.Fragment.FruitsFragment;
@@ -23,6 +25,19 @@ import com.codesnroses.foodo.Fragment.SeedsFragment;
 import com.codesnroses.foodo.Fragment.SweetsFragment;
 import com.codesnroses.foodo.Fragment.NavigationDrawerFragment;
 import com.codesnroses.foodo.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity
@@ -51,7 +66,38 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        postDeviceDataToServer();
     }
+
+    private void postDeviceDataToServer(){
+        //Get device unique id
+        String id = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
+
+
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://frodo.karlworks.com/api/devices/");
+        httppost.addHeader("Content-Type","application/json");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("uuid", id));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            Log.d("post","post successfully?");
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+    }
+
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
