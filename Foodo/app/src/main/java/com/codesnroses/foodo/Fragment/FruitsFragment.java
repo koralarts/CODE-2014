af930fd85eb50a19c6ea4d146575fc879882598e
Shim, Sybil <r_u_b_e_s@hotmail.com>
2015-02-22 14:11:25 -0800

    Refactor to share code more

--- Foodo/app/src/main/java/com/codesnroses/foodo/Fragment/FruitsFragment.java
+++ Foodo/app/src/main/java/com/codesnroses/foodo/Fragment/FruitsFragment.java
@@ -1,104 +1,23 @@
 package com.codesnroses.foodo.Fragment;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-
-import com.codesnroses.foodo.Activity.FatsDetail;
-import com.codesnroses.foodo.Adapter.ItemAdapter;
 import com.codesnroses.foodo.Model.Fats;
-import com.codesnroses.foodo.R;
-import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.regex.Pattern;
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FruitsFragment extends Fragment implements SearchView.OnQueryTextListener, AdapterView.OnItemSelectedListener {
-
-    private ItemAdapter itemAdapter;
-
-    private View rootView;
-    private ArrayList<Fats> fullList = new ArrayList<Fats>();
-    private ArrayList<Fats> adapterList = new ArrayList<Fats>();
-
-    private String searchText = "";
+public class FruitsFragment extends BaseFragment implements SearchView.OnQueryTextListener, AdapterView.OnItemSelectedListener {
 
     public FruitsFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        putFatsObjectIntoList();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        rootView = inflater.inflate(R.layout.fragment_fruits,container,false);
-
-        Log.d("Fruits Fragment", "creating fruits fragment now");
-
-        //Put all Fats object into the list View
-        itemAdapter = new ItemAdapter(getActivity(), adapterList);
-
-        attachListeners();
-
-        //Set adapter for the list view
-        ListView myItemList = (ListView)rootView.findViewById(R.id.listView_fruits);
-        myItemList.setAdapter(itemAdapter);
-
-        itemAdapter.notifyDataSetChanged();
-
-        return rootView;
-    }
-
-    //Sorting buttons
-    public void attachListeners(){
-        SearchView search = (SearchView) rootView.findViewById(R.id.search);
-        search.setOnQueryTextListener(this);
-        search.setQuery(searchText, false);
-
-
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_sort);
-        spinner.setOnItemSelectedListener(this);
-
-        ListView myItemList = (ListView) rootView.findViewById(R.id.listView_fruits);
-        myItemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Fats f = (Fats) adapterView.getItemAtPosition(i);
-
-                Intent intent = new Intent(rootView.getContext(), FatsDetail.class);
-
-                String selectedFatsJson = new Gson().toJson(f);
-                intent.putExtra("SelectedItem", selectedFatsJson);
-                startActivity(intent);
-            }
-        });
+        this.jsonFile = "fruits.json";
     }
 
     public void putFatsObjectIntoList(){
@@ -128,106 +47,9 @@ public class FruitsFragment extends Fragment implements SearchView.OnQueryTextLi
                 adapterList.add(f);
                 fullList.add(f);
             }
-        }catch(JSONException e){
+        } catch(JSONException e){
             e.printStackTrace();
             Toast.makeText(getActivity().getApplicationContext(),"Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
         }
-    }
-
-    /**
-     * Reads the fats.json file and returns the jsonArray object
-     * @return
-     */
-    private JSONArray getFatObjects(){
-        String jsonString = null;
-        JSONArray jsonArray = null;
-        try{
-            //open the input stream to the file
-            InputStream inputStream = getActivity().getAssets().open("fruits.json");
-
-            int size = inputStream.available();
-
-            //array that will store the data
-            byte[] bytes = new byte[size];
-
-            //reading data into the array from the file
-            inputStream.read(bytes);
-
-            //close the input stream
-            inputStream.close();
-
-            jsonString = new String(bytes,"UTF-8");
-            jsonArray = new JSONArray(jsonString);
-
-        }catch(IOException ex){
-            ex.printStackTrace();
-            return null;
-        }catch(JSONException x){
-            x.printStackTrace();
-            return null;
-        }
-        return jsonArray;
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        searchText = newText;
-
-        adapterList.clear();
-        if (newText == null || newText.isEmpty()) {
-            adapterList.addAll(fullList);
-        } else {
-            for (Iterator<Fats> it = fullList.iterator(); it.hasNext(); ) {
-                Fats item = it.next();
-                String foodName = item.getFood_name();
-                boolean found = Pattern.compile(Pattern.quote(newText), Pattern.CASE_INSENSITIVE).matcher(foodName).find();
-                if (found) {
-                    adapterList.add(item);
-                }
-            }
-        }
-
-        sortItems();
-
-        itemAdapter.notifyDataSetChanged();
-
-        return false;
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        sortItems();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    private void sortItems() {
-        Spinner sortView = (Spinner) rootView.findViewById(R.id.spinner_sort);
-
-        String selectedItem = sortView.getSelectedItem().toString();
-        switch (selectedItem) {
-            case "A-Z":
-                Collections.sort(adapterList, Fats.FatComparatorAtoZ);
-                break;
-            case "Z-A":
-                Collections.sort(adapterList, Fats.FatComparatorZtoA);
-                break;
-            case "Calories (Low to High)":
-                Collections.sort(adapterList, Fats.FatComparatorCalUp);
-                break;
-            case "Calories (High to Low)":
-                Collections.sort(adapterList, Fats.FatComparatorCalDown);
-                break;
-        }
-
-        itemAdapter.notifyDataSetChanged();
     }
 }
