27691bba168f9dc5ab3e258738ac8dfdcb3788e8
Shim, Sybil <r_u_b_e_s@hotmail.com>
2015-02-21 18:24:37 -0800

    Add filtering

--- Foodo/app/src/main/java/com/codesnroses/foodo/Fragment/GymFragment.java
+++ Foodo/app/src/main/java/com/codesnroses/foodo/Fragment/GymFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.codesnroses.foodo.Activity.GymActivity;
 import com.codesnroses.foodo.R;
@@ -36,12 +38,13 @@ import java.util.HashMap;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, AdapterView.OnItemSelectedListener {
 
     private static final LatLng CANADA = new LatLng(56.130366, -106.346771);
 
+    private GoogleMap map = null;
     private JSONArray clubListArray = null;
-    private HashMap<Marker, JSONObject> markerMap = new HashMap<Marker, JSONObject>();
+    private HashMap<Marker, JSONObject> markerMap = null;
 
     public GymFragment() {
     }
@@ -58,6 +61,11 @@ public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleM
     @Override
     public void onActivityCreated (Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinner_province);
+        spinner.setOnItemSelectedListener(this);
+        spinner = (Spinner) getActivity().findViewById(R.id.spinner_gender);
+        spinner.setOnItemSelectedListener(this);
 
         initMap();
     }
@@ -96,18 +104,34 @@ public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleM
         fm.getMapAsync(this);
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        map.setMyLocationEnabled(true);
-        map.setOnInfoWindowClickListener(this);
+    private void addMarkers() {
+        addMarkers(null, null);
+    }
 
+    private void addMarkers(String provinceFilter, String genderFilter) {
+        this.map.clear();
+        this.markerMap = new HashMap<Marker, JSONObject>();
         for (int i = 0, l = this.clubListArray.length(); i < l; i++) {
             try {
                 JSONObject club = this.clubListArray.getJSONObject(i);
                 String title = club.getString("publicName");
                 double lat = club.getDouble("latitude");
                 double lng = club.getDouble("longitude");
-                Marker marker = map.addMarker(new MarkerOptions()
+
+                if (provinceFilter != null) {
+                    String province = club.getString("province");
+                    if (!provinceFilter.equals(province)) {
+                        continue;
+                    }
+                }
+                if (genderFilter != null) {
+                    String gender = club.getString("gender");
+                    if (!genderFilter.equals(gender)) {
+                        continue;
+                    }
+                }
+
+                Marker marker = this.map.addMarker(new MarkerOptions()
                         .title(title)
                         .snippet(getResources().getString(R.string.gym_details))
                         .position(new LatLng(lat, lng)));
@@ -116,6 +140,16 @@ public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleM
                 Log.d("JSON Exception", e.toString());
             }
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+
+        map.setMyLocationEnabled(true);
+        map.setOnInfoWindowClickListener(this);
+
+        addMarkers();
 
         Location location = map.getMyLocation();
         if (location != null) {
@@ -136,5 +170,24 @@ public class GymFragment extends Fragment implements OnMapReadyCallback, GoogleM
         intent.putExtras(extras);
 
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+        filterMarkers();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parentView) {
+        filterMarkers();
+    }
+
+    private void filterMarkers() {
+        Spinner provinceView = (Spinner) getActivity().findViewById(R.id.spinner_province);
+        Spinner genderView = (Spinner) getActivity().findViewById(R.id.spinner_gender);
+
+        String provinceItem = provinceView.getSelectedItemPosition() == 0 ? null : provinceView.getSelectedItem().toString();
+        String genderItem = genderView.getSelectedItemPosition() == 0 ? null : genderView.getSelectedItem().toString();
+        addMarkers(provinceItem, genderItem);
     }
 }
